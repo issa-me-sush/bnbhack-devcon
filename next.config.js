96a5de72b61b80f35fc46eed765dcc75f113ac74
issa-me-sush <coolsushaan@gmail.com>
2024-11-10 06:54:54 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,42 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // your config options here
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      net: false,
-      os: false,
-      tls: false,
-      fs: false,
-      path: false,
-      buffer: require.resolve('buffer/'),
-    };
-
-    // Add rule for .wasm files
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-
-    return config;
-  },
-  // Ensure images from these domains are allowed
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.iconify.design',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ton.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'telegram.org',
-      },
-    ],
-  },
+  
 };
 
 module.exports = nextConfig;
