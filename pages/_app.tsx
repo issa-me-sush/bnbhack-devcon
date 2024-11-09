986d649ab477357baf260ad8aff87c149fe542cf
issa-me-sush <coolsushaan@gmail.com>
2024-11-10 06:34:54 +0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
-import type { AppProps } from 'next/app'
-import { TelegramProvider } from '@/providers/TelegramProvider'
-import '@/styles/globals.css'
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { TelegramProvider } from '@/providers/TelegramProvider';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <TelegramProvider>
-      <Component {...pageProps} />
-    </TelegramProvider>
-  )
+    <>
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="beforeInteractive"
+        crossOrigin="anonymous"
+      />
+      <TelegramProvider>
+        <Component {...pageProps} />
+      </TelegramProvider>
+    </>
+  );
 }
